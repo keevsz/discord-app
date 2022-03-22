a73712d0468dfd3e88b522b11d09343c88396594
keevsz <keviv1q2@gmail.com>
2022-03-22 13:58:02 -0500

    try1

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ const { isNotAuthorized } = require("../utils/auth")
 const router = Router()
 
 router.get("/", isNotAuthorized, (req, res) => {
-  res.render("Home")
+  res.render("home")
 })
 
 module.exports = router
